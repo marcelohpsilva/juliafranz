a483e859552d40d421e79b83b3cd8d5ce20b0352
marcelohpsilva <marcelohpsilva@gmail.com>
2025-04-14 21:54:56 -0700

    Adiciona basePath no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',       // Exportação estática
-  reactStrictMode: true,  // Modo estrito para React
-  distDir: 'out',         // Diretório de saída
+  output: 'export',        // Exportação estática
+  reactStrictMode: true,   // Modo estrito para React
+  distDir: 'out',          // Diretório de saída
+  basePath: '/nome-do-repositorio',
 };
 
 module.exports = nextConfig;
