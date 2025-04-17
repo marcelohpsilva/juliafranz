609289381e85932690ddea5ba79d5713fe13ae7c
marcelohpsilva <marcelohpsilva@gmail.com>
2025-04-17 16:43:41 -0700

    Feito
    Feito

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',        // Exportação estática
-  reactStrictMode: true,   // Modo estrito para React
-  distDir: 'out',          // Diretório de saída
-  basePath: '/nome-do-repositorio',
-};
-
-module.exports = nextConfig;
